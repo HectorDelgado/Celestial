20a1fc4c0c8e80f970dc1aaab6085ccf59dbee85
Hector Delgado <hector.d.delgado@gmail.com>
2024-11-02 16:28:07 -0700

    Remove unused files

--- composeApp/src/commonMain/kotlin/com/hectordelgado/celestial/actualexpect/StringFormatter.kt
+++
@@ -1,7 +0,0 @@
-    package com.hectordelgado.celestial.actualexpect
-
-interface StringFormatter {
-    fun format(string: String, vararg args: Any): String
-}
-
-expect fun getStringFormatter(): StringFormatter
--- composeApp/src/commonMain/kotlin/com/hectordelgado/celestial/db/AppDatabase.kt
+++ composeApp/src/commonMain/kotlin/com/hectordelgado/celestial/db/AppDatabase.kt
@@ -2,7 +2,7 @@ package com.hectordelgado.celestial.db
 
 import com.hectordelgado.celestial.db.dao.FavoriteImageOfTheDayDao
 
-class AppDatabase(private val favoriteImageOfTheDayDao: FavoriteImageOfTheDayDao) {
+class AppDatabase(val favoriteImageOfTheDayDao: FavoriteImageOfTheDayDao) {
     suspend fun clearDatabase() {
         favoriteImageOfTheDayDao.deleteAll()
     }
